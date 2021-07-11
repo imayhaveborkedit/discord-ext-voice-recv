63bb99f64761ae32afbd520d41162394d12e5b0a
Imayhaveborkedit <imayhaveborkedit@gmail.com>
2021-07-11 04:52:05 -0400

    Add "example"

---
+++ examples/recv.py
@@ -0,0 +1,12 @@
+# import discord
+# from discord.ext import voice_recv
+
+
+# def callback(member, packet):
+#     ...
+
+# vc = await channel.connect()
+# vc.listen(voice_recv.BasicSink(callback))
+
+# Something like this
+# TODO: a proper example
