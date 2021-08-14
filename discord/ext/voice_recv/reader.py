fc14106fb37199bc0d4a80fa9b743f5297bdfd5e
Imayhaveborkedit <imayhaveborkedit@gmail.com>
2021-08-14 00:29:14 -0400

    Add jitter buffer

--- discord/ext/voice_recv/reader.py
+++ discord/ext/voice_recv/reader.py
@@ -26,6 +26,7 @@ DEALINGS IN THE SOFTWARE.
 
 import time
 import wave
+import bisect
 import select
 import socket
 import audioop
@@ -547,3 +548,62 @@ class OpusEventAudioReader(_ReaderBase):
 
 
 AudioReader = OpusEventAudioReader
+
+
+class SimpleJitterBuffer:
+    """Push item in, returns as many contiguous items as possible"""
+
+    def __init__(self, maxsize=10, *, prefill=0):
+        if maxsize < 1:
+            raise ValueError('maxsize must be greater than 0')
+
+        self.maxsize = maxsize
+        self.prefill = prefill
+        self._last_seq = 0
+        self._buffer = []
+
+    def push(self, item):
+        if item.sequence <= self._last_seq and self._last_seq:
+            return []
+
+        bisect.insort(self._buffer, item)
+
+        if self.prefill > 0:
+            self.prefill -= 1
+            return []
+
+        return self._get_ready_batch()
+
+    def _get_ready_batch(self):
+        if not self._buffer or self.prefill > 0:
+            return []
+
+        if not self._last_seq:
+            self._last_seq = self._buffer[0].sequence - 1
+
+        # check to see if the next packet is the next one
+        if self._last_seq + 1 == self._buffer[0].sequence:
+
+            # Check for how many contiguous packets we have
+            n = ok = 0
+            for n in range(len(self._buffer)): # TODO: enumerate
+                if self._last_seq + n + 1 != self._buffer[n].sequence:
+                    break
+                ok = n + 1
+
+            # slice out the next section of the buffer
+            segment = self._buffer[:ok]
+            self._buffer = self._buffer[ok:]
+            if segment:
+                self._last_seq = segment[-1].sequence
+
+            return segment
+
+        # size check and add skips as None
+        if len(self._buffer) > self.maxsize:
+            buf = [None for _ in range(self._buffer[0].sequence-self._last_seq-1)]
+            self._last_seq = self._buffer[0].sequence - 1
+            buf.extend(self._get_ready_batch())
+            return buf
+
+        return []
