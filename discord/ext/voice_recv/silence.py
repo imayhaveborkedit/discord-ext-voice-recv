a84bf027ec2080e8a918973e58507c7927ecf3cd
Imayhaveborkedit <imayhaveborkedit@users.noreply.github.com>
2023-05-10 21:11:54 -0400

    Add silence.py with some boilerplate

---
+++ discord/ext/voice_recv/silence.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+import logging
+
+from typing import TYPE_CHECKING
+
+
+log = logging.getLogger(__name__)
+
+__all__ = [
+    'SilenceGenerator'
+]
+
+
+class SilenceGenerator:
+    def __init__(self):
+        ...
