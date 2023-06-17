e9d01b1ab4f0bab2669447b0e65a13ffbaec310f
Imayhaveborkedit <imayhaveborkedit@users.noreply.github.com>
2023-06-17 01:24:32 -0400

    Get rid of old todo text

--- discord/ext/voice_recv/__init__.py
+++ discord/ext/voice_recv/__init__.py
@@ -15,10 +15,3 @@ __author__ = 'Imayhaveborkedit'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2021-present Imayhaveborkedit'
 __version__ = '0.1.6'
-
-
-# TODO:
-#       Design reader protocol
-#       re-add all the client functions and state
-#       figure out the 'state' of speakingstate
-#       test
